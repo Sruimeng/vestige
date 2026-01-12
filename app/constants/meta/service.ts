bf08f52fd15d289499db1277273a814b5b2b9c37
Sruimeng <934274351@qq.com>
2026-01-13 03:06:53 +0800

    refactor(meta): 移除环境判断直接使用生产环境API和基础URL

--- app/constants/meta/service.ts
+++ app/constants/meta/service.ts
@@ -1,10 +1,2 @@
-import { isDEV } from './env';
-
-const DevApiURL = 'http://localhost:3001/api';
-const DevBaseUrl = 'http://localhost:3000';
-
-const ProdApiURL = 'https://api.sruim.xin';
-const ProdBaseUrl = 'https://vestige.sruim.xin';
-
-export const ApiURL = isDEV ? DevApiURL : ProdApiURL;
-export const BaseUrl = isDEV ? DevBaseUrl : ProdBaseUrl;
+export const ApiURL = 'https://api.sruim.xin';
+export const BaseUrl = 'https://vestige.sruim.xin';
